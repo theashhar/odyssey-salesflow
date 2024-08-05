9d246be2e6ef62e99cd1caaa6e054f12a671b65c
Utsav <utsavtiwari030@outlook.com>
2024-08-05 20:52:43 +0530

    added boiler plate

---
+++ main.js
@@ -0,0 +1,39 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+async function createWindow() {
+  const isDev = (await import('electron-is-dev')).default;
+
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  mainWindow.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, 'frontend/build/index.html')}`
+  );
+
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
